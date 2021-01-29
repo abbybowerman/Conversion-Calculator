33563db83ab233091013377f1f2c49ce0dca84a7
Abby Bowerman <abbybowerman@outlook.com>
2021-01-28 20:32:59 -0500

    added array to spinners, created units screen

---
+++ ConversionCalcApp/app/src/main/java/com/appdev/conversioncalculator/ChangeUnits.kt
@@ -0,0 +1,11 @@
+package com.appdev.conversioncalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeUnits : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_units)
+    }
+}
--- ConversionCalcApp/app/src/main/java/com/appdev/conversioncalculator/MainActivity.kt
+++ ConversionCalcApp/app/src/main/java/com/appdev/conversioncalculator/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.appdev.conversioncalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val changeUnitsButton = findViewById<Button>(R.id.changeUnitsButton)
+
+        val fromSpinner: Spinner = findViewById(R.id.fromUnits)
+        val toSpinner: Spinner = findViewById(R.id.toUnits)
+
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.length_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            fromSpinner.adapter = adapter
+            toSpinner.adapter = adapter
+        }
+
+        //set the default to value to centimeters
+        toSpinner.setSelection(4)
+
+        changeUnitsButton.setOnClickListener {
+            val intent = Intent(this, ChangeUnits::class.java)
+            startActivity(intent)
+        }
     }
 }
