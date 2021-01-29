b8bb4ef2188e8ed5e3d1e90bbf1dc9cad59286c8
Abby Bowerman <abbybowerman@outlook.com>
2021-01-28 22:06:17 -0500

    added spinner and button to change units screen

---
+++ ConversionCalcApp/app/src/main/java/com/appdev/conversioncalculator/Conversions.kt
@@ -0,0 +1,11 @@
+package com.appdev.conversioncalculator
+
+class Conversions {
+    fun calculateLength(to: Double, fromUnit: String, toUnit: String): Double{
+        return 0.0
+    }
+
+    fun calculateTemperature(to: Double, fromUnit: String, toUnit: String): Double{
+        return 0.0
+    }
+}
